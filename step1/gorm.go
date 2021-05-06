69dd81b79011b7d09cc63989599938b8570c4bf1
xinchengforgit <2419743144@qq.com>
2021-05-06 22:19:07 +0800

    Try to archieve the re-encapsulate of the sql

---
+++ step1/gorm.go
@@ -0,0 +1,41 @@
+package geeorm
+
+import (
+	"database/sql"
+
+	"geeorm/log"
+	"geeorm/session"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+//Engine的作用，连接数据库，返回*sql.DB
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	// 同时发送ping来检测是否连接上
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
+}
---
+++ step1/session/raw.go
@@ -0,0 +1,64 @@
+package session
+
+import (
+	"database/sql"
+	"geeorm/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+} //返回一个session
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+//清除sql的连接
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+//拼接各项sql的参数
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+//执行sql语句
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear() ///执行完之后清除session
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+//查询行
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+//s.sqlvars...表示被切片逐步传入
