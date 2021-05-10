bb034c36c9c8d898656784b81323ce7377d523db
xinchengforgit <2419743144@qq.com>
2021-05-10 23:48:44 +0800

    try to understand how to map the models to object

---
+++ step2/dialect/dialect.go
@@ -0,0 +1,19 @@
+package dialect
+
+import "reflect"
+
+var dialectsMap = map[string]Dialect{}
+
+type Dialect interface {
+	DataTypeOf(typ reflect.Value) string                    //用于将Go的类型转换为该数据库的类型
+	TableExistSQL(tableName string) (string, []interface{}) //返回某个表是否存在的 SQL 语句，参数是表名(table)
+}
+
+func RegisterDialect(name string, dialect Dialect) {
+	dialectsMap[name] = dialect
+}
+
+func GetDialect(name string) (dialect Dialect, ok bool) {
+	dialect, ok = dialectsMap[name]
+	return
+}
---
+++ step2/session/table_test.go
@@ -0,0 +1,23 @@
+package session
+
+// import (
+// 	"testing"
+// )
+
+// func TestSession_CreateTable(t *testing.T) {
+// 	s := NewSession().Model(&User{})
+// 	_ = s.DropTable()
+// 	_ = s.CreateTable()
+// 	if !s.HasTable() {
+// 		t.Fatal("Failed to create table User")
+// 	}
+// }
+
+// func TestSession_Model(t *testing.T) {
+// 	s := NewSession().Model(&User{})
+// 	table := s.RefTable()
+// 	s.Model(&Session{})
+// 	if table.Name != "User" || s.RefTable().Name != "Session" {
+// 		t.Fatal("Failed to change model")
+// 	}
+// }
